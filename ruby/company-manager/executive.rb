68068a75ee1073d19457fe206767679b3a5e12e7
Clay Diffrient <cdiffrient@instructure.com>
2015-01-30 23:48:29 -0700

    Adds executive class, needs specs

--- ruby/company-manager/executive.rb
+++ ruby/company-manager/executive.rb
@@ -1,3 +1,16 @@
 class Executive < Employee
+  attr_accessor :payrate
+
+  DEFAULT_PERCENTAGE  = .10
+
+  def initialize(name, percentage = DEFAULT_PERCENTAGE)
+    @percentage = percentage
+    super name
+  end
+
+  def pay(profits)
+    total_pay = profits * @percentage
+  end
+
 
 end
