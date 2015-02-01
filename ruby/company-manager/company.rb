2f32cc1e083439b56ca0e7e974328182ec707d85
Clay Diffrient <cdiffrient@instructure.com>
2015-02-01 07:58:25 -0700

    [company manager] Adds company class and more specs

---
+++ ruby/company-manager/company.rb
@@ -0,0 +1,29 @@
+require_relative './employee'
+
+class Company
+  attr_accessor :name, :employees
+
+  def initialize(name, employees = [])
+    @name = name
+    @employees = employees
+  end
+
+  def hire(employee)
+    @employees.push(employee)
+    @employees.count
+  end
+
+  def fire(employee)
+    if employee.is_a? String
+      employee_to_fire = @employees.find { |e| e.name == employee }
+    elsif employee.is_a? Numeric
+      employee_to_fire = @employees.find { |e| e.id == employee }
+    elsif employee.is_a? Employee
+      employee_to_fire = @employees.find { |e| e == employee }
+    end
+
+    @employees.delete(employee_to_fire)
+    employee_to_fire
+  end
+
+end
--- ruby/company-manager/company_spec.rb
+++ ruby/company-manager/company_spec.rb
@@ -9,6 +9,7 @@ describe "Company" do
 
   before :all do
     @test_company = Company.new "Test Company"
+    @manager = Manager.new "ClayManager"
   end
 
   describe "#initialize" do
@@ -16,39 +17,57 @@ describe "Company" do
       test = Company.new "Test Corp"
       expect(test).not_to be_nil
       expect(test.name).to eq("Test Corp")
+    end
   end
 
   describe "#hire" do
     it "should be able to hire an employee" do
-      employee = Employee.new "Clay"
+      employee = Employee.new "ClayEmployee"
       num_employees = @test_company.hire(employee)
       expect(num_employees).to eq(1)
     end
 
     it "should be able to hire an hourly employee" do
-      h_employee = HourlyEmployee.new "Clay"
+      h_employee = HourlyEmployee.new "ClayHourly"
       num_employees = @test_company.hire(h_employee)
       expect(num_employees).to eq(2)
     end
 
     it "should be able to hire a salaried employee" do
-      s_employee = SalariedEmployee.new "Clay"
+      s_employee = SalariedEmployee.new "ClaySalaried"
       num_employees = @test_company.hire(s_employee)
       expect(num_employees).to eq(3)
     end
 
     it "should be able to hire an executive" do
-      executive = Executive.new "Clay"
+      executive = Executive.new "ClayExecutive"
       num_employees = @test_company.hire(executive)
       expect(num_employees).to eq(4)
     end
 
     it "should be able to hire a manager" do
-      manager = Manager.new "Clay"
-      num_employees = @test_company.hire(manager)
+      num_employees = @test_company.hire(@manager)
       expect(num_employees).to eq(5)
     end
   end
 
+  describe "#fire" do
+    it "should be able to fire an employee by name" do
+      removed = @test_company.fire("ClayHourly")
+      expect(removed.name).to eq("ClayHourly")
+      expect(@test_company.employees.count).to eq(4)
+    end
 
+    it "should be able to fire an employee by id" do
+      removed = @test_company.fire(3)
+      expect(removed.name).to eq("ClaySalaried")
+      expect(@test_company.employees.count).to eq(3)
+    end
+
+    it "should be able to fire an employee given an employee" do
+      removed = @test_company.fire(@manager)
+      expect(removed).to eq(@manager)
+      expect(@test_company.employees.count).to eq(2)
+    end
+  end
 end
