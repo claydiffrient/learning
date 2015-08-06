c0724b5a3e0c0d13f94f2efbca0ede79ada0ed55
Clay Diffrient <cdiffrient@instructure.com>
2015-08-05 23:10:56 -0600

    Adds ability to scan directory and output a git blame

--- elixir/git_age/lib/git_age.ex
+++ elixir/git_age/lib/git_age.ex
@@ -8,11 +8,38 @@ defmodule GitAge do
 
   def process(options) do
     if (elem(options, 0)[:help]) do
-      IO.puts "git_age git_repo_path sha_to_check"
+      IO.puts "git_age git_repo_path"
     else
-      IO.puts "Not implemented yet"
+      open_directory(elem(options, 1))
     end
   end
+
+  def open_directory(path) do
+    File.cd!(path, fn(
+      ) ->
+      response = File.ls!(path)
+      Enum.each(response, fn(x) ->
+        IO.puts "Processing #{x}"
+        if (File.dir?(x)) do
+          #open_directory(x)
+          IO.puts "--Directory"
+        else
+          git_blame(x)
+        end
+      end)
+    end)
+  end
+
+  defp git_blame(file) do
+    files = []
+    results = System.cmd("git", ["blame", file],
+      into: files
+    )
+    Enum.each(elem(results, 0), fn(x) ->
+      IO.puts x
+    end)
+  end
+
 
 
   defp parse_args(args) do
