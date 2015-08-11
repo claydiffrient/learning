8f205231644d3c4e362fdfc617d7be92f0aadae9
Clay Diffrient <cdiffrient@instructure.com>
2015-08-10 22:22:36 -0600

    Begins restructuring

--- elixir/git_age/lib/git_age.ex
+++ elixir/git_age/lib/git_age.ex
@@ -1,32 +1,52 @@
 defmodule GitAge do
 
-
-
+  # Main
+  # Called from commandline
+  #
+  # Inputs:
+  #   - args - list of arguments from the commandline
+  # Outputs:
+  #   - None (other than a status code to the console)
   def main(args) do
     args |> parse_args |> process
   end
 
+  # Process
+  # Essentially where all arguments get sent for handling
+  #
+  # Inputs:
+  #   - options - Parsed arguments and options
+  # Outputs:
+  #   - None
   def process(options) do
     if (elem(options, 0)[:help]) do
       IO.puts "git_age git_repo_path"
     else
-      open_directory(elem(options, 1))
+      [head | tail] = elem(options, 1)
+      IO.inspect open_directory(head)
     end
   end
 
-  def open_directory(path) do
-    File.cd!(path, fn(
-      ) ->
+
+  # Open Directory
+  # Opens up the directory that was given by the user.  It will recursively
+  # crawl the directory structure until it can send back a full flat list of
+  # files.
+  #
+  # Inputs:
+  #   - path - the path that needs to be read
+  #   - files - a list of file strings, defaults to an empty list
+  # Outputs:
+  #   - List of files that need to be blamed.
+  def open_directory(path, files \\ []) do
+    File.cd!(path, fn() ->
       response = File.ls!(path)
-      counts = HashDict.new()
-      Dict.put(counts, "count", 0)
       Enum.each(response, fn(x) ->
-        IO.puts "Processing #{x}"
-        if (File.dir?(x)) do
-          #open_directory(x)
-          IO.puts "--Directory"
+        full_path = path <> "/" <> x
+        if (File.dir?(full_path)) do
+          open_directory(full_path, files)
         else
-          git_blame(x, counts);
+          List.insert_at(files, -1, full_path)
         end
       end)
     end)
