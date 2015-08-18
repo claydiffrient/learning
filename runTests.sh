a2470498d387f701cb0e1996ece64f2f0d24d237
Clay Diffrient <cdiffrient@instructure.com>
2015-08-17 23:20:28 -0600

    Makes elixir tests run as part of the test suite for the repo

--- runTests.sh
+++ runTests.sh
@@ -3,5 +3,16 @@
 # TODO: Allow a single test to be run from here.
 ###
 
+echo 'Running ruby tests'
 cd ruby
 rspec
+
+echo 'Running elixir tests'
+cd ../elixir
+for elixir_project in *; do
+  if [ -d ${elixir_project} ]; then
+    cd $elixir_project
+    mix test
+    cd ..
+  fi
+done
