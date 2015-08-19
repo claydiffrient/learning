e2a0393cf0754ad6f81c3ee11f05062d811ca765
Clay Diffrient <cdiffrient@instructure.com>
2015-08-18 22:37:53 -0600

    Adds test for opening directories

--- elixir/git_age/test/git_age_test.exs
+++ elixir/git_age/test/git_age_test.exs
@@ -1,7 +1,14 @@
 defmodule GitAgeTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "reads in files in the fixtures directory" do
+    fixtures_dir = File.cwd!() <> "/test/fixtures"
+    expected_files = [
+      fixtures_dir <> "/firstLevelFile.txt",
+      fixtures_dir <> "/firstLevelFolder/secondLevelFile.txt",
+      fixtures_dir <> "/firstLevelFolder/secondLevelFolder/thirdLevelFile.txt"
+    ]
+    actual_files = GitAge.open_directory(fixtures_dir)
+    assert actual_files == expected_files
   end
 end
